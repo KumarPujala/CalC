8330145dc79046472bb03d2f4bfafc6afd1e339e
kumar pujala <65699454+KumarPujala@users.noreply.github.com>
2022-01-16 23:14:20 +0530

    Update RoundButton.swift

--- CalC/CalC/RoundButton.swift
+++ CalC/CalC/RoundButton.swift
@@ -14,13 +14,13 @@ class RoundButton: UIButton {
     @IBInspectable var roundButton: Bool = false {
         didSet{
             if roundButton{
-                layer.cornerRadius = frame.height / 2
+                layer.cornerRadius = frame.height 
             }
         }
     }
     override func prepareForInterfaceBuilder() {
         if roundButton{
-            layer.cornerRadius = frame.height/2  
+            layer.cornerRadius = frame.height
         }
     }
 }
